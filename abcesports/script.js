5eac177e2cba5cacb0cf4881b6d2356efede394f
Arthur Lola Oliveira <arthurlolaoliveira@hotmail.com>
2023-08-14 20:04:18 -0300

    adicionei o carrossel

---
+++ abcesports/script.js
@@ -0,0 +1,18 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    proxImg();
+}, 5000)
+
+
+function proxImg(){
+    count++;
+
+    if(count > 6){
+        count =  1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+
+}
